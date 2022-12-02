9c345c39d387640e17c2c9a22fd84c8bce229cdf
Arvind <arvind.gidde28@gmail.com>
2022-12-02 16:29:00 +0530

    Added Java File

---
+++ A1.java
@@ -0,0 +1,9 @@
+class Assignemt1{
+    int a=2;
+    int b=3;
+    int c=a+b;
+    public static void public static void main(String[] args) {
+        System.out.println(c);
+        System.out.println("Hello");
+    }
+}
